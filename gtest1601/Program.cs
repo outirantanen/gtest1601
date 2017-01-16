47f040b5c751fadc5f1492a15930923bcb7f8640
Outi Rantanen <K1496@student.jamk.fi>
2017-01-16 19:20:59 +0200

    test

---
+++ gtest1601/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest1601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("jhgljdahgljdahlg");
+            Console.WriteLine("jhgljdahgljdahlg");
+        }
+    }
+}
